605e7378ca4def31c83dadb19e396b22e2640474
SupaStuff <StuffQilla@gmail.com>
2018-06-03 12:04:19 -0400

    added docker-compose

--- app/docker.sh
+++ app/docker.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+DOCKER_RELEASE=""
+COMPOSE_RELEASE="1.21.2"
+
 curl -fsSL https://download.docker.com/linux/debian/gpg \
    | gpg --dearmor \
    > /etc/apt/trusted.gpg.d/docker.gpg
@@ -7,3 +10,7 @@ echo "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -c
    > /etc/apt/sources.list.d/docker.list
 apt-get update
 apt-get install -y docker-ce
+
+sudo curl -L https://github.com/docker/compose/releases/download/$COMPOSE_RELEASE/docker-compose-$(uname -s)-$(uname -m) \
+          -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
